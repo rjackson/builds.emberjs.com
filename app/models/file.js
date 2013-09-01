e570b42fff67f2260d53a970e9eceeec9ea7955b
Robert Jackson <robert.w.jackson@me.com>
2013-09-01 00:40:36 -0400

    Whitespace.

--- app/models/file.js
+++ app/models/file.js
@@ -7,7 +7,7 @@ var S3File = Ember.Object.extend({
 
   url: function(){
     return this.get('baseUrl') + '/' + this.get('relativePath');
-  }.property('baseUrl', 'relativePath') 
+  }.property('baseUrl', 'relativePath')
 });
 
 export default S3File;
